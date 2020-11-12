65fcc9e3ddf8532add65250dc27b382e70d68798
Elias Ersson <32256443+Eliaznizzle@users.noreply.github.com>
2020-11-12 15:35:30 +0100

    anotha one

--- src/CarTransport.java
+++ src/CarTransport.java
@@ -11,12 +11,30 @@ public class CarTransport extends Truck implements ICarTransport {
         return platformLowered;
     }
 
+    @Override
     public void loadCar(Car car) {
-        if (!platformLowered() || )
+        if (!platformLowered() || getDistance(car) > 5 || car == this){
+            //Car cannot be loaded because the platform is raised, the car is too far, or this transport is trying to load itself
+            return;
+        }
         loadedCars.push(car);
     }
 
+    @Override
     public Car unloadCar() {
-        return loadedCars.pop();
+        Car car = loadedCars.pop();
+        car.x -= Math.cos(getAngle()) * 2;
+        car.y -= Math.sin(getAngle()) * 2;
+        return car;
+    }
+
+    @Override
+    public void move() {
+        super.move();
+        for (Car car : loadedCars){
+            car.x = x;
+            car.y = y;
+            car.set
+        }
     }
 }
