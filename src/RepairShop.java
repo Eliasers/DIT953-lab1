afe87790ed8cbbcb39c3c199a9339ab7188876c7
Zeeta00 <69895254+Zeeta00@users.noreply.github.com>
2020-11-12 16:11:00 +0100

    Update RepairShop.java

--- src/RepairShop.java
+++ src/RepairShop.java
@@ -1,32 +1,34 @@
-import java.util.Deque;
+import java.util.ArrayList;
+
 /**
  * A repair shop that is able to take in a specific numbers of cars and later on let them out.
  */
-public class RepairShop implements ICarTransport{
-    private int maxNumberOfCars;
-    private Deque<Car> brokenCars;
+public class RepairShop {
+    int maxNumberOfCars;
+    private final ArrayList<Car> brokenCars = new ArrayList<>(10);
 
 
     /**
      * @param car Puts car in the repair shop
      */
-    @Override
     public void loadCar(Car car) {
-        brokenCars.push(car);
+        brokenCars.add(car);
     }
 
     /**
-     * @return Removes car from the repair shop
+     * @return Removes car from the repair shop by index nr, if the number given currently is in repair shop
      */
-    @Override
-    public Car unloadCar() {
-        return brokenCars.pop();
+    public Car unloadCar(int indexNr) {
+        if (indexNr >= 0 && indexNr < maxNumberOfCars) {
+            return brokenCars.remove(0);
+        }
+        return null;
     }
 
     /**
      * Constructs a Repair shop with default values
      */
-    public RepairShop(){
+    public RepairShop() {
         maxNumberOfCars = 12;
     }
 
