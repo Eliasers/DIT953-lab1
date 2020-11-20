771174c6e2cf8aac8cdd73693b799588d5c5f2d1
Elias Ersson <32256443+Eliaznizzle@users.noreply.github.com>
2020-11-20 13:38:25 +0100

    Generalized repairshop code

--- src/VolvoShop.java
+++ src/VolvoShop.java
@@ -1,14 +1,11 @@
 public class VolvoShop extends RepairShop {
 
     public void deposit(Volvo240 car){
-        if (cars.size() == capacity) return;
-        cars.add(car);
+        super.deposit(car);
     }
 
-    Volvo240 retrieve(int index){
-        Volvo240 car = (Volvo240)cars.get(index);
-        cars.remove(car);
-        return car;
+    public Volvo240 retrieve(int index){
+        return (Volvo240)super.retrieve(index);
     }
 
 }
